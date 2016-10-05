b96201892fc9fe3c778a93f727f898b846c3f64a
LncMug <lunaticlight123@gmail.com>
2016-10-05 20:45:03 +0900

    solved build issue

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/lunalynn/drone-pj-2016aw/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/src:$ROS_PACKAGE_PATH"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages"
