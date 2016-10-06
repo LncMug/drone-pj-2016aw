20a25ccb984f7b22e4e06ba7a79fa011bb5f39cd
LncMug <lunaticlight123@gmail.com>
2016-10-06 14:14:58 +0900

    build error: server/client

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,5 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/lunalynn/drone-pj-2016aw/catkin_ws/build"
-export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages"
+export PYTHONPATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lunalynn/drone-pj-2016aw/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/lunalynn/drone-pj-2016aw/catkin_ws/src:$ROS_PACKAGE_PATH"
